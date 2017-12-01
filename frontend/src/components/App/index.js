57c3fbf824598d4537bc82327c15f1c31e3d725a
shotor <shotor@shotor.com>
2017-12-01 13:45:50 +0100

    move socket io to component

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -3,12 +3,62 @@ import React, { Component } from 'react';
 import { WeatherToday} from '../WeatherToday';
 import { WeatherForecast } from '../WeatherForecast';
 
+import io from 'socket.io-client';
+
 export class App extends Component {
     constructor() {
         super();
         this.state = {
             weather: {},
         }
+    }
+
+    componentDidMount() {
+      const conn = io('http://192.168.1.46:3000');
+      conn.on('weather/location', res => {
+        window.navigator.geolocation.getCurrentPosition(res => {
+          const { coords } = res
+          conn.emit('weather/today', coords)
+        })
+      })
+      conn.on('weather/today', (weather) => { 
+        this.setState({ weather })
+      })
+      conn.on("connect", () => {
+        console.log("[EVENT][CONNECTION] Connected to server");
+      });
+      
+      conn.on("connect_error", err => {
+        console.log("[ERROR][CONNECTION]", err);
+      });
+      
+      conn.on("connect_timeout", err => {
+        console.log("[ERROR][CONNECTION] Timeout", err);
+      });
+      
+      conn.on("disconnect", reason => {
+        console.log("[EVENT][CONNECTION] Disconnecting");
+      });
+      
+      conn.on("error", err => {
+        console.log(`[ERROR]`, err);
+      });
+      
+      conn.on("reconnect_attempt", () => {
+        console.log("[EVENT][RECONNECT] Reconnecting");
+      });
+      
+      conn.on("reconnect_error", err => {
+        console.log("[ERROR][RECONNECT]", err);
+      });
+      
+      conn.on("reconnect_failed", err => {
+        console.log("[ERROR][RECONNECT][FAILED]", err);
+      });
+      
+      conn.on("reconnect", attempts => {
+        console.log(`[EVENT][RECONNECT] Reconnected after ${attempts} attempts`);
+      });
     }
 
     render() {
